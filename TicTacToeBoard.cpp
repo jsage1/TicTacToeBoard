2625456ddc47e367df04637293b9e0a2663120fd
Justin A Sage <jsage1@ecc-jaguar.csuchico.edu>
2019-11-20 11:40:10 -0800

    added test

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -19,7 +19,14 @@ TicTacToeBoard::TicTacToeBoard()
 **/
 Piece TicTacToeBoard::toggleTurn()
 {
-  return Invalid;
+	if(turn == 'X')
+	{
+		turn == 'O';
+	}else
+	{
+		turn == 'X';
+	}
+  return turn;
 }
 
 /**
